ac477a97f7a8177d09eccfcf5cdd82f642ebd69d
Matheus <matheus.uspeu@gmail.com>
2021-06-13 22:08:27 -0300

    responsive menu with angular material

---
+++ MenuResponsivoMaterial/menuResponsive/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from './material.module';
+
+@NgModule({
+  declarations: [			
+    AppComponent,
+      LoginComponent,
+      RegisterComponent,
+      NavBarComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
