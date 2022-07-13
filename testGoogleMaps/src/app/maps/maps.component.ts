f65613f2fecfcce3311ebb1d02b8fba058c67508
Matheus <matheus.uspeu@gmail.com>
2022-07-12 23:45:22 -0300

    maps modificado

--- testGoogleMaps/src/app/maps/maps.component.ts
+++ testGoogleMaps/src/app/maps/maps.component.ts
@@ -1,6 +1,5 @@
 import { MapsAPILoader } from '@agm/core';
-import { Component, OnInit } from '@angular/core';
-import { GetGeolocationService } from '../services/get-geolocation.service';
+import { Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-maps',
@@ -9,27 +8,67 @@ import { GetGeolocationService } from '../services/get-geolocation.service';
 })
 export class MapsComponent implements OnInit {
 
-  constructor(private geoLocationService: GetGeolocationService, private mapsAPILoader: MapsAPILoader) { }
+  title: string = 'AGM project';
+  markerTitle: string = 'Localização Atual';
+  latitude: number;
+  longitude: number;
+  zoom: number;
+  address: string;
+  private geoCoder: any;
+
+  @ViewChild('search')
+  public searchElementRef: ElementRef;
+
+  constructor(private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone) { }
 
   ngOnInit(): void {
-     this.initMap();
+      this.initMap();
   }
-
-  title: string = 'My first AGM project';
-  markerTitle: string = 'Localização atual';
-  lat: number;
-  lng: number;
-  zoom: number = 13;
-  coordinates: any;
-
-  map: any;
 
   initMap(): void {
-    this.geoLocationService.getPosition().subscribe(
-      (pos) => {
-          this.lat = pos.coords.latitude;
-          this.lng = pos.coords.longitude
-      });
+    this.mapsAPILoader.load().then(() => {
+      this.setCurrentLocation();
+      this.geoCoder = new google.maps.Geocoder;
+    }).catch((error) => {
+       console.log(error)
+    });
   }
+
+  private setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      const watchID = navigator.geolocation.watchPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 8;
+        this.getAddress(this.latitude, this.longitude);
+        navigator.geolocation.clearWatch(watchID);
+        console.log(watchID)
+      }, (error) => {
+         console.log(error);
+      }, {enableHighAccuracy: true, timeout: 0, maximumAge: 0});
+
+
+    }
+  }
+
+  getAddress(latitude: number, longitude: number) {
+    this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results: any, status: string) => {
+      if (status === 'OK') {
+        if (results[0]) {
+          this.zoom = 12;
+          this.address = results[0].formatted_address;
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+
+    });
+  }
+
+
+
 
 }
